7fb56955a92a971c90b76651fedc3653ec794565
miagerman <49194476@est.ort.edu.ar>
2025-06-05 10:11:43 -0300

    Initial commit

---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+const config = {
+    host : "ep-wild-resonance-a8669jkm-pooler.eastus2.azure.neon.tech",
+    database : "neondb",
+    user : "alumno_ort",
+    password : "T1Crules25!",
+    port : 5432,
+    ssl : true,
+    }
+    export default config;
+    uhwdhiwh
